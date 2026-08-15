0460a24ee1ae0123fb3af3892e4b67a91ac7ed9c
AlbyAtkinson71 <albyatkinson@hotmail.com>
2026-08-15 22:08:48 +0100

    Updating speedinsights tag

--- app/layout.js
+++ app/layout.js
@@ -21,6 +21,7 @@ export default function RootLayout({ children }) {
         <Navigation />
         {children}
         <Analytics />
+        <SpeedInsights />
 
         <footer className="bg-gray-900 text-white py-12 mt-16">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
